1076e2f8b519113d5731291d336119d92500f087
itamar <itamarcu@gmail.com>
2022-12-08 00:27:57 +0200

    improved day 7 solution code a little

--- day7.py
+++ day7.py
@@ -2,19 +2,19 @@ import dataclasses
 from typing import List
 
 
+@dataclasses.dataclass
 class Thing:
+    name: str
     pass
 
 
 @dataclasses.dataclass
 class File(Thing):
-    name: str
     size: int
 
 
 @dataclasses.dataclass
 class Directory(Thing):
-    name: str
     contents: List[Thing]
 
     def calc_size(self) -> int:
@@ -32,42 +32,31 @@ with open('day7.txt') as input_file:
     input_lines = input_file.readlines()
     input_lines = [line.strip('\n') for line in input_lines]
 
-
 top_level_directory = Directory(name='/', contents=[])
 current_path: List[Directory] = [top_level_directory]  # index -1 is current directory
 
-line_i = -1
-while line_i + 1 < len(input_lines):
-    line_i += 1
-    line = input_lines[line_i]
+for line in input_lines:
     current_directory = current_path[-1]
     if line.startswith('$ cd /'):
         current_path[-1] = top_level_directory
-        continue
-    if line.startswith('$ cd ..'):
+    elif line.startswith('$ cd ..'):
         current_path.pop()
-        continue
-    if line.startswith('$ cd '):  # (+name)
-        name_of_inner_directory = line[len('$ cd '):]
-        subdirectory = next(d for d in current_directory.contents if d.name == name_of_inner_directory and isinstance(d, Directory))
-        # TODO ***
+    elif line.startswith('$ cd '):  # (+name)
+        inner_dir = line[len('$ cd '):]
+        subdirectory = next(d for d in current_directory.contents if d.name == inner_dir and isinstance(d, Directory))
         current_path.append(subdirectory)
-        continue
-    if line.startswith('$ ls'):
-        # add next lines as subcontents
-        while line_i + 1 < len(input_lines) and not input_lines[line_i + 1].startswith('$'):
-            line_i += 1
-            # keep adding
-            line = input_lines[line_i]
-            part_1, part_2 = line.split(' ')
-            if part_1 == 'dir':
-                new_directory = Directory(name=part_2, contents=[])
-                current_directory.contents.append(new_directory)
-            else:
-                new_file = File(name=part_2, size=int(part_1))
-                current_directory.contents.append(new_file)
-        continue
-    print(f'SHOULD NEVER GET HERE')
+    elif line.startswith('$ ls'):
+        # will add next lines as subcontents
+        # handled in the following "else" branch
+        pass
+    else:  # line does not start with $ at all, and is in fact just one of the `ls` outputs
+        part_1, part_2 = line.split(' ')
+        if part_1 == 'dir':
+            new_directory = Directory(name=part_2, contents=[])
+            current_directory.contents.append(new_directory)
+        else:
+            new_file = File(name=part_2, size=int(part_1))
+            current_directory.contents.append(new_file)
 
 
 THRESHOLD = 100000
@@ -105,4 +94,3 @@ def calculate_minimum_size_necessary_recursively(dir: Directory):
 
 
 print(calculate_minimum_size_necessary_recursively(top_level_directory))  # 3636703
-
