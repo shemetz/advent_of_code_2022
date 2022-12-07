a494ba9cf6c5bd68f2aac09aa26b3708c0d31578
itamar <itamarcu@gmail.com>
2022-12-07 19:36:54 +0200

    improved readability of day 6

--- day6.py
+++ day6.py
@@ -10,17 +10,17 @@ def solve(window_size: int):
     actual_window_size = window_size - 1
     chars_in_window = defaultdict(lambda: 0)  # will always contain actual_window_size letters
     for i in range(actual_window_size):
-        c = big_line[i]
-        chars_in_window[c] += 1
+        chars_in_window[big_line[i]] += 1
     for i in range(len(big_line) - window_size):
-        c = big_line[i + actual_window_size]
-        if chars_in_window[c] == 0:
+        left_i = i
+        right_i = i + actual_window_size
+        if chars_in_window[big_line[right_i]] == 0:
             if all(value <= 1 for value in chars_in_window.values()):
                 # we're done
-                print(i + actual_window_size + 1)  # 1343
+                print(right_i + 1)  # 1343
                 break
-        chars_in_window[big_line[i]] -= 1
-        chars_in_window[c] += 1
+        chars_in_window[big_line[left_i]] -= 1
+        chars_in_window[big_line[right_i]] += 1
 
 
 solve(4)
